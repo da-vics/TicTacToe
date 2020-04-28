bfe9db2124362eefd7da6ef66a614ab71c88168c
Tomoloju Victor <tomoloju45@gmail.com>
2020-04-28 00:32:29 -1100

    not done

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -20,14 +20,99 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        #region privateMembers
+
+        /// <summary>
+        /// instance of <see cref="TicTac"/>
+        /// </summary>
+        private TicTac ticTac;
+        /// <summary>
+        /// initialise new game
+        /// </summary> 
+
+        private void newGame()
         {
-            InitializeComponent();
+            ticTac.defaultTileInit();
 
             Container.Children.Cast<Button>().ToList().ForEach(button =>
             {
 
+                button.Content = string.Empty;
+                button.Background = Brushes.White;
+                button.Foreground = Brushes.Blue;
             });
         }
+
+        /// <summary>
+        /// button click Event Handler....
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// 
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+
+            var column = Grid.GetColumn(button);
+            var row = Grid.GetRow(button);
+
+            var gridIndex = column + (row * 3);
+
+            if (ticTac.PlayerState && ticTac.GameState)
+            {
+                if (ticTac.tileValues[gridIndex] != TicTac.BoxState.free)
+                {
+                    MessageBox.Show("Already Filled!");
+                    /// return;
+                }
+
+                else
+                {
+                    ticTac.tileValues[gridIndex] = TicTac.BoxState.cross;
+                    button.Content = 'X';
+                    ticTac.PlayerState = false;
+                }
+                ticTac.checkGameState();
+            }
+
+
+            if (!ticTac.PlayerState && ticTac.GameState)
+            {
+
+                var btn = Container.Children.Cast<Button>().ToArray();
+                var index = ticTac.computerPlay();
+                btn[index].Content = "O";
+                btn[index].Foreground = Brushes.Red;
+                ticTac.tileValues[index] = TicTac.BoxState.zero;
+                ticTac.PlayerState = true;
+            }
+
+            ticTac.getWinner();
+
+            if (!ticTac.GameState)
+            {
+                Container.Children.Cast<Button>().ToList().ForEach(button =>
+                {
+                    button.Background = Brushes.Gold;
+                    button.Foreground = Brushes.Black;
+
+                });
+            }
+        }
+
+        #endregion
+
+        #region publicMembers
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ticTac = new TicTac();
+
+            newGame();
+        }
+
+        #endregion
+
     }
 }
---
+++ TicTacToe/TicTac.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class TicTac
+    {
+
+        #region privateMembers
+        /// <summary>
+        /// maxsize of tilevales array <see cref="tileValues"/>
+        /// </summary>
+        public int maxSize { get; private set; } = 9;
+        #endregion
+
+        #region publicVariables
+
+        public int maxRowSize { get; private set; } = 3;
+        public int maxcolSize { get; private set; } = 3;
+
+        public bool PlayerState { get; set; } = true;
+
+        public bool GameState { get; set; } = true;
+
+        /// <summary>
+        /// state of gridBox
+        /// </summary>
+        public enum BoxState
+        {
+            /// empty game tile
+            free,
+            /// game tile with X
+            cross,
+            /// game tile with O
+            zero
+        }
+
+        public BoxState[] tileValues;
+
+        public TicTac() => this.tileValues = new BoxState[9];
+
+        public void defaultTileInit()
+        {
+
+            for (int i = 0; i < maxSize; ++i)
+            {
+                tileValues[i] = BoxState.free;
+            }
+        }
+
+        public int computerPlay()
+        {
+
+            int index = 0;
+
+            while (true)
+            {
+                Random random = new Random();
+                index = random.Next(0, 8);
+
+                if (tileValues[index] == BoxState.free)
+                    break;
+            }
+
+            return index;
+        }
+
+        public void checkGameState()
+        {
+            foreach (var tile in this.tileValues)
+            {
+                if (tile == TicTac.BoxState.free)
+                {
+                    this.GameState = true;
+                    break;
+                }
+                else
+                {
+                    this.GameState = false;
+                }
+
+            }
+        }
+
+
+        public void getWinner()
+        {
+            var temp = this.maxRowSize;
+            var temp2 = 0;
+            bool foundPattern = false;
+            for (var j = 0; j < this.maxRowSize; ++j)
+            {
+                for (var i = temp2; i < temp; ++i)
+                {
+                    if (tileValues[i] != BoxState.cross)
+                    {
+                        this.GameState = true;
+                        break;
+                    }
+
+                    else if (tileValues[i] == BoxState.cross)
+                    {
+                        this.GameState = false;
+                        foundPattern = true;
+                    }
+                }
+
+                if (foundPattern)
+                    break;
+
+                temp += this.maxRowSize;
+                temp2 += 2 + 1;
+            }
+
+
+        }
+        #endregion
+
+    }
+
+}
