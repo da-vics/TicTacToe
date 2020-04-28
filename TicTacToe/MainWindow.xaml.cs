aa3b54acbd91a6858580d5ec25dcfe3297d44c83
Tomoloju Victor <tomoloju45@gmail.com>
2020-04-28 02:36:36 -1100

    Game logic implemented

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TicTacToe
 {
@@ -96,7 +86,6 @@ namespace TicTacToe
                     {
                         button.Background = Brushes.Gold;
                         button.Foreground = Brushes.Black;
-
                     });
                 }
 
